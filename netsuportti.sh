b17568f6f5fda20c0bec192d0d6cb4d7bee1b4ad
Rafael Tavares <rafael@ibinetwork.com.br>
2019-04-24 20:49:43 -0300

    Update netsuportti.sh

--- netsuportti.sh
+++ netsuportti.sh
@@ -27,5 +27,10 @@ cowsay "PATCH-BR EXECUTADO COM SUCESSO"
 sleep 5
 chmod +x /usr/src/Netsuportti/update_language_theme.sh
 /usr/src/Netsuportti/update_language_theme.sh
+sed -i -r 's/33697B/134372/' /var/www/html/modules/pbxadmin/themes/default/css/mainstyle.css
+sed -i -r 's/33697B/134372/' /var/www/html/admin/assets/css/mainstyle.css
+rm -Rf /usr/src/Netsuportti
 clear
+echo ""
 cowsay "SCRIPT NETSUPORTTI EXECUTADO COM SUCESSO!
+echo ""
